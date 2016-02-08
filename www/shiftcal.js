d3f6813619fa0511d2c2ef8f34ccea508b4df9ee
Elliott Eggleston <ejegg@ejegg.com>
2016-02-07 16:57:52 -0800

    Sort by and format event time
    Also remove sample data from HTML

--- www/shiftcal.js
+++ www/shiftcal.js
@@ -14,9 +14,29 @@ $(document).ready( function() {
 				};
 				mustacheData.dates.push(groupedByDate[date]);
 			}
+			var timeParts = value.time.split(':');
+			var hour = parseInt(timeParts[0]);
+			var meridian = 'AM';
+			if ( hour > 12 ) {
+				hour = hour - 12;
+				meridian = 'PM';
+			}
+			value.displayTime = hour + ':' + timeParts[1] + ' ' + meridian;
 			groupedByDate[date].events.push(value);
 		});
 
+		var compareEvents = function ( event1, event2 ) {
+			if ( event1.time < event2.time ) {
+				return -1;
+			}
+			if ( event1.time > event2.time ) {
+				return 1;
+			}
+			return 0;
+		};
+		for ( var date in groupedByDate )  {
+			groupedByDate[date].events.sort(compareEvents);
+		}
 		var template = $('#mustache-template').html();
 		var info = Mustache.render(template, mustacheData);
 		$('#mustache-html').append(info);
