be57beb4896a90120fac5532d3f5c97d36a58a36
Vincent Khougaz <vincent@khougaz.com>
2016-04-10 15:57:30 -0700

    Integrated date picker into edit form, removed cal-build.html

--- www/shiftcal.js
+++ www/shiftcal.js
@@ -93,6 +93,8 @@ $(document).ready( function() {
         var template = $('#mustache-edit').html();
         var info = Mustache.render(template, shiftEvent);
         $('#mustache-html').empty().append(info);
+        setupDatePicker(shiftEvent['dates'] || []);
+
         $('#edit-header').affix({
             offset: {
                 top: 100,
@@ -101,6 +103,7 @@ $(document).ready( function() {
         $('#save-button').click(function() {
             var postVars = {};
             $('form').serializeArray().map(function(x){postVars[x.name] = x.value;}) ;
+            postVars['dates'] = dateList();
             $.ajax({
                 type: 'POST',
                 url: 'manage_event.php',
@@ -116,6 +119,196 @@ $(document).ready( function() {
             });
         });
     }
+
+    /* Date Picker JS */
+    // Global state variables, initialized in setupDatePicker
+    var $dateSelect,
+        $loadLater,
+        $loadEarlier,
+        $monthTable,
+        monthTemplate,
+        earliestMonth,
+        latestMonth,
+        dateMap,
+        today;
+
+    // Some constants used for generating html. The JOY of javascript stdlib
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    var cellClases = ["color-jan", "color-feb", "color-mar", "color-apr", "color-may", "color-jun",
+        "color-jul", "color-aug", "color-sep", "color-oct", "color-nov", "color-dec"];
+
+    // dateList returns the currently selected list of (normalized) dates for consumption by manage_event.php
+    function dateList() {
+        var dates = [];
+        for (var key in dateMap) {
+            if (dateMap.hasOwnProperty(key) && dateMap[key]) {
+                dates.push(key);
+            }
+        }
+        return dates;
+    }
+
+    // setupDatePicker sets up the global variables and populates the date picker element
+    function setupDatePicker(dates) {
+        // Fill in global variables
+        // Set up dateMap
+        dateMap = {};
+        for (var i=0; i<dates.length; i++) {
+            dateMap[normalizeDate(dates[i])] = true;
+        }
+
+        // Scrolling container for the table
+        $dateSelect = $('#date-select');
+        // Placeholder divs that trigger loading when visible
+        $loadLater = $('#load-later');
+        $loadEarlier = $('#load-earlier');
+        // Table that contains months
+        $monthTable = $("#month-table");
+
+        monthTemplate = $('#mustache-select-month').html();
+
+        // Tracks when today is for styling purposes
+        today = new Date();
+        // These track which month to add to the start/end
+        earliestMonth = today;
+        latestMonth = today;
+
+        // Fill in the first month
+        $monthTable.html(getMonthHTML(earliestMonth));
+        // Scroll the table to the top of the first month
+        $dateSelect.scrollTop(loadLaterTop());
+
+        // Add a click handler for individual days
+        $monthTable.click(function(ev) {
+            var e = ev.target;
+            if (e.hasAttribute('data-date')) {
+                var $e = $(e),
+                    date = $e.attr('data-date');
+
+                dateMap[date] = !dateMap[date];
+                $e.toggleClass('selected', dateMap[date]);
+                return false;
+            }
+            return true;
+        });
+
+        // Setup the month table scroll checks
+        $dateSelect.scroll(checkBounds);
+    }
+
+
+    function isToday(date) {
+        return (date.getDate() == today.getDate()
+            && date.getMonth() == today.getMonth()
+            && date.getFullYear() == today.getFullYear())
+    }
+
+    function normalizeDate(date) {
+        var jsd = new Date(date);
+        var day = jsd.getDate();
+        var monthIndex = jsd.getMonth();
+        var year = jsd.getFullYear();
+
+        return year + '-' + (monthIndex+1) + '-' + day;
+    }
+
+    function isSelected(date) {
+        return !!dateMap[normalizeDate(date)];
+    }
+
+    function makeWeekData(date) {
+        // date is the first day in the week
+        // [{ day - string to display
+        //    classes - classes to display },...]
+        var week = [];
+        for (var i=0;i<7;i++) {
+            var day = {};
+            day['day'] = date.getDate();
+            day['date'] = normalizeDate(date);
+            day['classes'] = (isToday(date) ? "today" : "") + " " + (isSelected(date) ? "selected" : "") + " " + cellClases[date.getMonth()] + (date.getDay() % 2 == 0 ? "-odd" : "");
+            week.push(day);
+
+            date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
+        }
+        return week;
+    }
+
+    function makeMonthData(date) {
+        // date is a day in a month to add
+
+        // weeks:
+        //   monthTitle - left title, only in first week
+        //   weeksInMonth - rows that the title spans
+        //   days:
+        //     day: title
+        //     date: yyyy-m-d (normalized)
+        //     selected: cell class if selected
+
+        // Normalize date, ensuring first of month
+        var firstOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+        // Find the first day of the week for the week the 1st falls on
+        var firstDay = new Date(firstOfMonth.getFullYear(), firstOfMonth.getMonth(), -firstOfMonth.getDay()+1);
+
+        var firstOfNextMonth = new Date(date.getFullYear(), date.getMonth()+1, 1);
+        // Don't include the week next months 1st lands on
+        var stopDay = new Date(firstOfNextMonth.getFullYear(), firstOfNextMonth.getMonth(), -firstOfNextMonth.getDay()+1);
+
+        var weeks = [];
+        for (var startOfWeek=firstDay;startOfWeek<stopDay;) {
+            var week = {};
+            week['days'] = makeWeekData(startOfWeek);
+            weeks.push(week);
+
+            startOfWeek = new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 7);
+        }
+        weeks[0]['monthTitle'] = monthNames[firstOfMonth.getMonth()] + " " + firstOfMonth.getFullYear();
+        weeks[0]['weeksInMonth'] = weeks.length;
+
+        return {"weeks": weeks};
+    }
+
+    function getMonthHTML(date) {
+        return Mustache.render(monthTemplate, makeMonthData(date));
+    }
+
+    function loadLaterTop() {
+        return $loadLater.offset().top - $dateSelect.offset().top;
+    }
+
+    function loadEarlierBottom() {
+        return ($loadEarlier.offset().top + $loadEarlier.prop('scrollHeight')) - $dateSelect.offset().top;
+    }
+
+    var checking = false;
+    function checkBounds() {
+        if (checking) {
+            return;
+        }
+        var added = false;
+        if (loadEarlierBottom() >= 0) {
+            latestMonth = new Date(latestMonth.getFullYear(), latestMonth.getMonth()-1, 1);
+            var preHeight = $monthTable.height();
+            $monthTable.prepend(getMonthHTML(latestMonth));
+            var heightChange = $monthTable.height() - preHeight;
+            $dateSelect.scrollTop($dateSelect.scrollTop() + heightChange);
+            added = true;
+        }
+        if (loadLaterTop() <= $dateSelect.height()) {
+            earliestMonth = new Date(earliestMonth.getFullYear(), earliestMonth.getMonth()+1, 1);
+            $monthTable.append(getMonthHTML(earliestMonth));
+            added = true;
+        }
+        if (added) {
+            checking = true;
+            setTimeout(function() {
+                checking = false;
+                checkBounds();
+            }, 10);
+        }
+    }
+    /* /Date Picker JS */
 
     displayCalendar();
 });
